712ff56f2e0b88758c2f2342e5f81f250f90b0fd
筒井理美 <ttistm4062@yahoo.co.jp>
2019-09-25 21:35:40 +0900

    修正

--- Assets/Destroy.cs
+++ Assets/Destroy.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour {
 
-	// Use this for initialization
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+
+    //Use this for initialization
 	void Start () {
-		
-	}
+
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
 
-    void OnBecameInvisible()
-    {
-        Destroy(this.gameObject);
+        if (unitychan.transform.position.z - 10 > this.gameObject.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+
     }
+
+    //void OnBecameInvisible()
+    //{
+    //    Destroy(this.gameObject);
+    //}
 }
